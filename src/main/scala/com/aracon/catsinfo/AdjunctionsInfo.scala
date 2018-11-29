ae504d43483d9831f40d36a9b16e1dffb299def5
Pere Villega <pvillega@aracon.com>
2018-11-29 13:50:23 +0000

    Notes on adjunctions

---
+++ src/main/scala/com/aracon/catsinfo/AdjunctionsInfo.scala
@@ -0,0 +1,17 @@
+package com.aracon.catsinfo
+
+object AdjunctionsInfo {
+
+  // Adjunctions are among the most important constructions in mathematics.
+  // - First, every fundamental datatype— sums, products, function types, recursive types—arises out of an adjunction.
+  //   The defining properties of an adjunction give rise to well-known laws of the algebra of programming.
+  // - Second, adjunctions are instrumental in unifying and generalising recursion schemes.
+
+  // They are present on many structures, as can be seen at https://www.youtube.com/watch?v=h60VMBPfLoM
+  // But currently it seems to be more of a theoretical foundation that helps generating other constructions
+  // than a typeclass we can directly apply.
+  //
+  // Even if we would us Adjunctions to build, for example, our own State monads, it would be much less performant than
+  // the State monad implementations we currently use. So, good for lawfulness, theoretical basis of constructs, but
+  // not really applicable right now.
+}
